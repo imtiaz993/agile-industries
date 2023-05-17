dd936c481f47985c56e39baaff4d5ede6254d255
imtiaz hussain <imtiazh993@gmail.com>
2023-05-17 08:43:48 -0700

    initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+    <h1 className="text-3xl text-red-600">
+      AGILE INDUSTRIES
+    </h1>
+    </div>
+  );
+};
+
+export default Home;
